4d84a5650feeaf1af20bbddecaa6b82cdb37eaee
Sambhav Jain <sambhavjain233@gmail.com>
2022-04-24 00:14:52 +0530

    deploy on heroku

--- index.js
+++ index.js
@@ -77,6 +77,6 @@ app.get("/logout",function(req,res){
 
 
 //listen port ----------------------------------------------------------------------------------------------------
-app.listen(3000,function(){
-    console.log("server started on port 3000");
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
